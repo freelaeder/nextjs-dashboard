4e8f2c2a979cbccc6875843f5b23d5e0ea9c2598
freelaeder <yanglo67@qq.com>
2025-04-24 18:11:27 +0800

    feat: 更新发票页面，集成分页、搜索功能，优化布局和样式

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true; // Enable PPR for this layout
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
